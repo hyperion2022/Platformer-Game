3ac3cc2294b7b89ef8fba499295b81e882a40ca2
Mihai Anca <ancamihai2018@yahoo.com>
2022-06-28 10:47:07 +0300

    Standard locomotion in place

--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -7,7 +7,7 @@ public class characterMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Camera moveCamera;
-    public float smooth = 10f;
+    public float smoothRotation = 10f;
     Animator animator;
     PlayerInput input;
 
@@ -16,10 +16,15 @@ public class characterMovement : MonoBehaviour
     Vector3 movement;
     Vector2 lookValue;
 
+    // smooth dampening variables
     private Vector2 currentInputVector;
     private Vector2 smoothInputVelocity;
     [SerializeField]
     private float smoothInputSpeed = .2f;
+
+    // variable to store optimized setter/getter parameter IDs
+    int isRunningHash;
+    int isCrouchingHash;
 
     // Awake is called when the script instance is being loaded
     void Awake()
@@ -28,14 +33,41 @@ public class characterMovement : MonoBehaviour
 
         input.CharacterControls.Movement.performed += ctx =>
         {
+            // read WASD as Vector2
             inputDirection = ctx.ReadValue<Vector2>();
         };
         input.CharacterControls.Movement.canceled += _ =>
         {
+            // no input => no movement
             inputDirection = Vector2.zero;
         };
-        input.CharacterControls.Rotate.performed += ctx => {
+        input.CharacterControls.Rotate.performed += ctx =>
+        {
+            // read Mouse Position as Vector2
             lookValue = ctx.ReadValue<Vector2>();
+        };
+        input.CharacterControls.Run.performed += ctx =>
+        {
+            // if crouching, uncrouch
+            if (animator.GetBool(isCrouchingHash))
+            {
+                animator.SetBool(isCrouchingHash, false);
+            }
+            // start running
+            animator.SetBool(isRunningHash, true);
+        };
+        input.CharacterControls.Run.canceled += ctx =>
+        {
+            // stop running
+            animator.SetBool(isRunningHash, false);
+        };
+        input.CharacterControls.Crouch.performed += ctx =>
+        {
+            // if not running, crouch or uncrouch
+            if (!animator.GetBool(isRunningHash))
+            {
+                animator.SetBool(isCrouchingHash, !animator.GetBool(isCrouchingHash));
+            }
         };
     }
 
@@ -44,6 +76,10 @@ public class characterMovement : MonoBehaviour
     {
         // set the animator reference
         animator = GetComponent<Animator>();
+
+        // set the ID references
+        isRunningHash = Animator.StringToHash("isRunning");
+        isCrouchingHash = Animator.StringToHash("isCrouching");
     }
 
     // Update is called once per frame
@@ -53,25 +89,27 @@ public class characterMovement : MonoBehaviour
         handleRotation();
     }
 
-    void handleRotation()
-    {
-        Vector3 direction = new Vector3(lookValue.x, 0f, lookValue.y).normalized;
-
-        if (direction.magnitude >= 0.1f && movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, moveCamera.transform.eulerAngles.y, 0), Time.deltaTime * smooth);
-        }
-    }
-
     void handleMovement()
     {
+        // direction vector smoothening
         currentInputVector = Vector2.SmoothDamp(currentInputVector, inputDirection, ref smoothInputVelocity, smoothInputSpeed);
         movement = new Vector3(currentInputVector.x, currentInputVector.y, 0.0f);
 
+        // set parameter values in animator
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
     }
 
+    void handleRotation()
+    {
+        Vector3 direction = new Vector3(lookValue.x, 0f, lookValue.y).normalized;
+
+        // if player is moving, change direction using the mouse
+        if (direction.magnitude >= 0.1f && movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, moveCamera.transform.eulerAngles.y, 0), Time.deltaTime * smoothRotation);
+        }
+    }
 
     void OnEnable()
     {
